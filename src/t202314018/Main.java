8174424c1d5350e05cd13a446d5c9bec317145ab
Kim, Yewon <ch0c0kan326@gmail.com>
2025-03-25 21:40:58 +0900

    fifth

--- src/t202314018/Main.java
+++ src/t202314018/Main.java
@@ -5,6 +5,7 @@ public class Main {
 	public static void main(String[] args) {
 		System.out.println("김예원");
 		World.run();
+		World.run2();
 
 	}
 
--- src/t202314018/World.java
+++ src/t202314018/World.java
@@ -5,4 +5,8 @@ public class World {
     public static void run(){
         System.out.println("202314018");
     }
+
+    public static void run2(){
+        System.out.println("hello world");
+    }
 }
